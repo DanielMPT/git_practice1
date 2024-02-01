18db70e8033cfa2bb0f28e13d9cb8244acb7c3db
Daniel Marin <136021712+DanielMPT@users.noreply.github.com>
2024-02-01 16:34:51 -0500

    Adding a multiplication to the original script

--- git_prueba1.py
+++ git_prueba1.py
@@ -1,7 +1,11 @@
 dic = [1,2,3,4,5,6,7,8,9]
 
 squared = [i**2 for i in dic if i % 2 == 0]  # 
- 
-print(squared)
 
 
+# Multiplication
+
+one_to_ten = [x for x in range(1, 11)]
+
+multiplication = [x * y for x, y in zip(squared, one_to_ten)]
+print(multiplication)
